d22fdb81656648ab6aff97913e21bb70fcf66c92
sloast <67313765+sloast@users.noreply.github.com>
2022-02-07 09:35:56 +0000

    added dash and buffers

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,8 +12,14 @@ public class PlayerMovement : MonoBehaviour
     public float FallSpeed;
     public float FallAccel;
     public float JumpSpeed;
-    public bool canJump;
-    public bool canAbility;
+    public float dashSpeed;
+    bool canJump = true;
+    float leftGroundBuffer = 0f;
+    bool canAbility = true;
+    float jumpBuffer = 0f;
+    public float maxJumpBuffer = .1f;
+    bool dashNextFrame = false;
+    Vector2 dashDirection;
 
     public bool onGround;
     Rigidbody2D rb;
@@ -28,10 +34,13 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
 
-        // Jump
-        if (onGround && Input.GetKeyDown("c"))
+        if (Input.GetKeyDown("c"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, JumpSpeed);
+            StartJump();
+        }
+        if (Input.GetKeyDown("x"))
+        {
+            StartDash();
         }
     }
 
@@ -41,15 +50,67 @@ public class PlayerMovement : MonoBehaviour
         // Default movement
         MoveHorizontal();
         MoveVertical();
+        CheckGrounded();
+        CheckBuffer();
+    }
 
-        // Check if the player just landed
+    void CheckGrounded()
+    {
+        
         bool temp = onGround;
         onGround = isGrounded();
-        if (!temp && onGround)
+        if (!temp && onGround) // Check if the player just landed
         {
             ResetAbilities();
         }
-        
+        else if (!onGround) // Check if the player just left the ground
+        {
+            if (temp)
+            {
+                leftGroundBuffer = .1f; // Give a short period of time for the player to jump
+            } else {
+                leftGroundBuffer -= Time.fixedDeltaTime;
+                if (leftGroundBuffer <= 0)
+                {
+                    canJump = false;
+                }
+            }
+
+        }
+        CheckBuffer();
+    }
+
+    // Check whether an input is queued
+    void CheckBuffer()
+    {
+        if (dashNextFrame)
+        {
+            Dash();
+        }
+
+        if (jumpBuffer > 0)
+        {
+            if (canJump)
+            {
+                Jump();
+            } else {
+                jumpBuffer -= Time.fixedDeltaTime;
+            }
+        }
+    }
+
+    // Queues a jump to be executed on the next FixedUpdate, or when the player next touches the ground.
+    void StartJump()
+    {
+        jumpBuffer = maxJumpBuffer;
+    }
+
+    void Jump()
+    {
+        jumpBuffer = 0f;
+        canJump = false;
+        float speed_y = JumpSpeed;
+        rb.velocity = new Vector2(rb.velocity.x, speed_y);
     }
 
     // Get arrow keys input in a more useful format
@@ -83,11 +144,26 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    void StartDash()
+    {
+        dashNextFrame = true;
+        int inputX = GetAxis("x");
+        int inputY = GetAxis("y");
+        if (inputX == 0 && inputY == 0){
+            inputX = 1;
+        }
+        dashDirection = new Vector2(inputX, inputY).normalized;
+    }
+
     void Dash()
     {
+        dashNextFrame = false;
+        if (!canAbility) { return; }
         canAbility = false; // Stop the player from using until landing
-        rb.velocity = new Vector2(rb.velocity.x, JumpSpeed); // Currently just a second jump
-        Invoke("ResetAbilitiesIfGrounded", 1); // Allows it to reset without the player needing to jump
+
+        rb.velocity = dashDirection * dashSpeed;
+        
+        Invoke("ResetAbilitiesIfGrounded", .5f); // Allows it to reset without the player needing to jump
     }
 
     // Controls gravity
