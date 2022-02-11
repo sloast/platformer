dab8a68e0d0a7ce89463b2aee93855f2daec4117
sloast <67313765+sloast@users.noreply.github.com>
2022-02-11 11:48:57 +0000

    change PlayerMovement to Player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector3 target;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void SetTarget(Vector3 newTarget)
+    {
+        target = newTarget;
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Player player;
+    new CameraController camera;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Player>();
+        camera = GameObject.FindWithTag("MainCamera").GetComponent<CameraController>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void ChangeLevel()
+    {
+
+    }
+}
